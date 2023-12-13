ccba54737ef851c4a48648995eb35cc854a60a0c
TimekillerTK <erwartungen@protonmail.com>
2023-12-13 13:06:29 +0100

    Removed spaghetti test config, changed home-manager syntax

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "Nixos config flake";
 
   inputs = {
+    # Unstable nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # For VS Code Remote to work on NixOS
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # For Home Manager
+    home-manager.url = "github:nix-community/home-manager"; 
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, vscode-server, ... }@inputs:
@@ -19,17 +21,6 @@
     {
     
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./configuration.nix
-            inputs.home-manager.nixosModules.default
-            vscode-server.nixosModules.default
-            ({ config, pkgs, ... }: {
-             services.vscode-server.enable = true;
-            })
-          ];
-        };
-      nixosConfigurations.spaghetti = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
             ./configuration.nix
