340e9763d77a64a5c7be589415f00dfc174f25e4
TimekillerTK <erwartungen@protonmail.com>
2023-12-15 14:32:55 +0100

    rearranged homemanager config

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      # libx = import ./lib { inherit self inputs outputs stateVersion hmStateVersion; };
+      libx = import ./lib { inherit self inputs outputs stateVersion hmStateVersion; };
     in
     {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
