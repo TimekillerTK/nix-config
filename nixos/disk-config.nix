efa51a1640703e3b502cb5ab899c1c15354656a3
TimekillerTK <erwartungen@protonmail.com>
2024-01-05 10:25:56 +0100

    (?) uncommenting boot partition causes error

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -1,41 +1,43 @@
 {lib, ...}: {
   disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = lib.mkDefault "/dev/sda";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            size = "1M";
-            type = "EF02"; # for grub MBR
-          };
-          ESP = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      main = {
+        type = "disk";
+        device = lib.mkDefault "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            # boot = {
+            #   size = "1M";
+            #   type = "EF02"; # for grub MBR
+            # };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-          root = {
-            end = "-6G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
-          };
-          plainSwap = {
-            size = "100%";
-            content = {
-              type = "swap";
-              resumeDevice = true; # resume from hiberation from this device
-            };
-          };
-        };
-      };
-    };
-  };
+            # plainSwap = {
+            #   size = "100%";
+            #   content = {
+            #     type = "swap";
+            #     resumeDevice = true; # resume from hiberation from this device
+            #   };
+            # }; # plainSwap
+          }; # partitions
+        }; # content 
+      }; # main
+    }; # disk
+  }; # disko.devices
 }
