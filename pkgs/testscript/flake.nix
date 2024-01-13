de641d4b206694bfc4d8930fe5813b9826ee7346
TimekillerTK <erwartungen@protonmail.com>
2024-01-13 10:54:18 +0100

    (?) Testing custom shellscript package

--- pkgs/testscript/flake.nix
+++ pkgs/testscript/flake.nix
@@ -1,5 +1,6 @@
+# Example from: https://www.ertt.ca/nix/shell-scripts/
 {
-  description = "A simple script";
+  description = "A simple test script";
 
   outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.testscript;
@@ -7,10 +8,19 @@
 
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.writeShellScriptBin "testscript" ''
-        DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
-        ${pkgs.cowsay}/bin/cowsay Hello, world! Today is $DATE.
-      '';
+
+        my-name = "testscript";
+        my-src = builtins.readFile ./testscript.sh;
+        my-script = (pkgs.writeScriptBin my-name my-src).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+        my-buildInputs = with pkgs; [ cowsay ddate ];
+
+      in pkgs.symlinkJoin {
+        name = my-name;
+        paths = [ my-script ] ++ my-buildInputs;
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+      };
   };
 }
