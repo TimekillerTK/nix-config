627ed7722aa992348c3abafa417eb18ae2821c7d
TimekillerTK <erwartungen@protonmail.com>
2024-01-18 19:54:17 +0100

    (?) WIP Config, testing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,18 +76,18 @@
   # This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  # Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     tk = {
-      # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
       initialPassword = "Hello123!";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        (builtins.readFile ./mbp.pub)
+        (builtins.readFile ./anya.pub)
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel"];
     };
   };
