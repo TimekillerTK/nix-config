f585aa1d3227d9a2eed53fc0324ea27179202953
= <=>
2024-01-21 08:35:28 +0000

    (working) Moved nix configs to common setting

--- hosts/nix-test/default.nix
+++ hosts/nix-test/default.nix
@@ -1,5 +1,3 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
   outputs,
@@ -8,7 +6,6 @@
   pkgs,
   ...
 }: {
-  # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
@@ -18,17 +15,16 @@
     # inputs.hardware.nixosModules.common-ssd
     inputs.vscode-server.nixosModules.default
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
+    # Generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix 
+
+    # Others
+    ../common/nix
   ];
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
+      # Flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.other-packages
@@ -43,71 +39,40 @@
       #   });
       # })
     ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-  };
-
-  # VS Code Server Module
-  services.vscode-server.enable = true;
-
-  networking.hostName = "nix-test";
-
-  # This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.grub = {
     # efiSupport = true;
     # efiInstallAsRemovable = true;
     devices = [ "/dev/sda" ];
   };
 
+  # VS Code Server Module (for VS Code Remote)
+  services.vscode-server.enable = true;
+
+  networking.hostName = "nix-test";
+
   # Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     tk = {
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
       initialPassword = "Hello123!";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        (builtins.readFile ./mbp.pub)
-        (builtins.readFile ./anya.pub)
+        (builtins.readFile ../common/ssh/mbp.pub)
+        (builtins.readFile ../common/ssh/anya.pub)
       ];
-      # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel"];
     };
   };
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
+  # SSH Config
   services.openssh = {
     enable = true;
     settings = {
-      # Forbid root login through SSH.
       PermitRootLogin = "no";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
       PasswordAuthentication = false;
     };
   };
