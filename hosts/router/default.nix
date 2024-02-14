631e7d44b6d8ea545ce5577568379edd1cb9a481
TimekillerTK <erwartungen@protonmail.com>
2024-02-14 11:16:52 +0100

    (working) Router config

--- hosts/router/default.nix
+++ hosts/router/default.nix
@@ -5,7 +5,11 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+let 
+  wanPort = "eth1";
+  lanPort = "eno1";
+in {
 
   imports = [
     ../common/global
@@ -16,9 +20,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # TODO: Ensure firewall is started BEFORE Network interfaces come up
+  # use default bash
+  users.users.tk.shell = lib.mkForce pkgs.bash;
 
-  # For network troubleshooting (remove later ?)
+  # For network troubleshooting
   environment.systemPackages = with pkgs; [
     tcpdump
     nmap
@@ -31,9 +36,7 @@
     "net.ipv4.conf.all.forwarding" = true;
   };
 
-  # use default bash
-  users.users.tk.shell = lib.mkForce pkgs.bash;
-
+  # Configuring Network Interfaces
   systemd.network = {
     enable = true;
 
@@ -44,9 +47,9 @@
     wait-online.anyInterface = true;
 
     networks = {
-      # NIC1 `eth1` - `DHCP` (WAN)
+      # NIC1 (WAN)
       "10-wan" = {
-        matchConfig.Name = "eth1";
+        matchConfig.Name = wanPort;
         networkConfig = {
           DHCP = "ipv4";
           IPForward = true;
@@ -56,9 +59,9 @@
         # make routing on this interface a dependency for network-online.target
         linkConfig.RequiredForOnline = "routable";
       };
-      # NIC2 `eno1` - `172.17.0.1/16` (LAN)
+      # NIC2 (LAN)
       "20-lan" = {
-        matchConfig.Name = "eno1";
+        matchConfig.Name = lanPort;
         address = [
           "172.17.0.1/16"
         ];
@@ -70,6 +73,7 @@
     };
   };
 
+  # Mostly Firewall Rules
   networking = {
     hostName = "router";
 
@@ -88,19 +92,102 @@
 
       # Actual NFTables rules
       ruleset = ''
-        table ip whatever {
-          chain thingscomingintohost {
-            type filter hook input priority 0; policy accept;
-            ct state related,established accept
-            iifname eth1 drop
+        # Source: https://oxcrag.net/projects/linux-router-part-1-routing-nat-and-nftables/
+        # Our future selves will thank us for noting what cable goes where and labeling the relevant network interfaces if it isn't already done out-of-the-box.
+        define WANPORT = ${wanPort}
+        define LANPORT = ${lanPort}
+
+        # We never expect to see the following address ranges on the Internet
+        define BOGONS4 = { 
+          0.0.0.0/8, 
+          10.0.0.0/8, 
+          10.64.0.0/10,
+          127.0.0.0/8,
+          127.0.53.53,
+          169.254.0.0/16,
+          172.16.0.0/12,
+          192.0.0.0/24,
+          192.0.2.0/24,
+          192.168.0.0/16,
+          198.18.0.0/15,
+          198.51.100.0/24,
+          203.0.113.0/24,
+          224.0.0.0/4,
+          240.0.0.0/4,
+          255.255.255.255/32
+        }
+        
+        table inet filter {
+          chain inbound_world {
+            # Drop obviously spoofed inbound traffic (to turn on later)
+            # ip saddr { $BOGONS4 } drop
           }
-          chain lastminutemods {
-            type nat hook postrouting priority 0; policy accept;
-            oifname eth1 masquerade
+
+          chain inbound_private {
+            # We want to allow remote access over ssh, incoming DNS traffic, and incoming DHCP traffic
+	        	ip protocol . th dport vmap { tcp . 22 : accept, udp . 53 : accept, tcp . 53 : accept, udp . 67 : accept }
+	        }
+
+          chain inbound {
+            # Default Deny
+            type filter hook input priority 0; policy drop;
+
+            # Allow established and related connections: Allows Internet servers to respond to requests from our Internal network
+            ct state vmap { established : accept, related : accept, invalid : drop} counter
+
+            # ICMP is - mostly - our friend. Limit incoming pings somewhat, but allow necessary information.
+            icmp type echo-request counter limit rate 5/second accept
+            ip protocol icmp icmp type { destination-unreachable, echo-reply, echo-request, source-quench, time-exceeded } accept
+
+            # Drop obviously spoofed loopback traffic
+            iifname "lo" ip daddr != 127.0.0.0/8 drop
+
+            # Separate rules for traffic from Internet and from the internal network
+            iifname vmap { lo: accept, $WANPORT : jump inbound_world, $LANPORT : jump inbound_private }
+          }
+
+          # Rules for sending traffic from one network interface to another
+          chain forward {
+            # Default deny, again
+            type filter hook forward priority 0; policy drop;
+
+            # Accept established and related traffic
+            ct state vmap { established : accept, related : accept, invalid : drop }
+
+            # Let traffic from this router and from the Internal network get out onto the Internet
+            iifname { lo, $LANPORT } accept
+          }
+        }
+
+        # Network address translation: What allows us to glue together a private network with the Internet even though we only have one routable address, as per IPv4 limitations
+        table ip nat {
+          chain postrouting {
+            type nat hook postrouting priority 100; policy accept;
+
+            # Pretend that outbound traffic originates in this router so that Internet servers know where to send responses
+            oifname $WANPORT masquerade
           }
         }
       '';
     };
+  };
+
+  # Ensure networkd waits for nftables (firewall) to start first
+  # Plus other wants/after which were there by default
+  systemd.services.systemd-networkd = {
+    wants = [ 
+      "nftables.service"
+      "systemd-networkd.socket"
+      "network.target"
+    ];
+    after = [ 
+      "nftables.service"
+      "systemd-networkd.socket"
+      "systemd-udevd.service"
+      "network-pre.target"
+      "systemd-sysusers.service"
+      "systemd-sysctl.service"
+    ];
   };
   
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
