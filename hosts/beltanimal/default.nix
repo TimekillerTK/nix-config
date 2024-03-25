cbe75d0c02818603158efe656f60e20eafd4e6ec
TimekillerTK <erwartungen@protonmail.com>
2024-03-25 20:18:18 +0100

    (wip) removed tailscale temporarily

--- hosts/beltanimal/default.nix
+++ hosts/beltanimal/default.nix
@@ -83,14 +83,15 @@
   sops.secrets.smbcred = { };
   sops.secrets.tailscale = { };
 
-  # Tailscale
-  services.tailscale = {
-    enable = true;
-    authKeyFile = "/run/secrets/tailscale";
-    extraUpFlags = [
-      "--advertise-tags=tag:usermachine"
-    ];
-  };
+  # TODO: This is busted, needs a fix
+  # # Tailscale
+  # services.tailscale = {
+  #   enable = true;
+  #   authKeyFile = "/run/secrets/tailscale";
+  #   extraUpFlags = [
+  #     "--advertise-tags=tag:usermachine"
+  #   ];
+  # };
 
   # Hostname & Network Manager
   networking.hostName = "beltanimal";
