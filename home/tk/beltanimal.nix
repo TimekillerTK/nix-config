c0e7c30426503d080ac837b910d5eb1dd008df71
TimekillerTK <erwartungen@protonmail.com>
2024-03-26 17:38:49 +0100

    (wip) Adding steam

--- home/tk/beltanimal.nix
+++ home/tk/beltanimal.nix
@@ -53,6 +53,9 @@
     (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" ]; }) # only 1 font
   ];
 
+  # Steam
+  programs.steam.enable = true;
+
   programs.home-manager.enable = true;
   home.stateVersion = "23.11"; # Please read the comment before changing.
 }
