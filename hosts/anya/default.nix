3cdd38344efa386635068f845fd16a6e2a37319c
TimekillerTK <erwartungen@protonmail.com>
2024-04-02 21:03:36 +0200

    (working) added anya smb mount

--- hosts/anya/default.nix
+++ hosts/anya/default.nix
@@ -22,7 +22,7 @@
     # Repo Modules
     ../common/global
     ../common/users/tk
-#    ../common/optional/sops
+    ../common/optional/sops
     ../common/optional/zfs
     ../common/optional/kde-plasma-x11
   ];
@@ -47,9 +47,9 @@
   services.vscode-server.enable = true;
 
   # Actual SOPS keys
-#  sops.defaultSopsFile = ./secrets.yml;
-#  sops.secrets.smbcred = { };
-#  sops.secrets.tailscale = { };
+  sops.defaultSopsFile = ./secrets.yml;
+  sops.secrets.smbcred = { };
+  # sops.secrets.tailscale = { };
 
   # TODO: This is busted, needs a fix
   # # Tailscale
@@ -79,16 +79,15 @@
     nvd # Nix/NixOS package version diff tool
   ];
 
-  # TODO: later
-  # # Mounting fileshare
-  # fileSystems."/mnt/FreeNAS" = {
-  #   device = "//freenas.cyn.internal/mediasnek2";
-  #   fsType = "cifs";
-  #   # TODO: UID should come from the user dynamically
-  #   # noauto + x-systemd.automount - disables mounting this FS with mount -a & lazily mounts (when first accessed)
-  #   # Remember to run `sudo umount /mnt/FreeNAS` before adding/removing "noauto" + "x-systemd.automount"
-  #   options = [ "credentials=/run/secrets/smbcred" "noserverino" "rw" "_netdev" "uid=1000"] ++ ["noauto" "x-systemd.automount"];
-  # };
+  # Mounting fileshare
+  fileSystems."/mnt/FreeNAS" = {
+    device = "//freenas.cyn.internal/mediasnek2";
+    fsType = "cifs";
+    # TODO: UID should come from the user dynamically
+    # noauto + x-systemd.automount - disables mounting this FS with mount -a & lazily mounts (when first accessed)
+    # Remember to run `sudo umount /mnt/FreeNAS` before adding/removing "noauto" + "x-systemd.automount"
+    options = [ "credentials=/run/secrets/smbcred" "noserverino" "rw" "_netdev" "uid=1000"] ++ ["noauto" "x-systemd.automount"];
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
