2f9db324d57b0d941fe9a720ab392a57631e35b6
TimekillerTK <erwartungen@protonmail.com>
2024-04-16 20:41:11 +0200

    (working) enable bluetooth for beltanimal

--- hosts/beltanimal/default.nix
+++ hosts/beltanimal/default.nix
@@ -64,6 +64,10 @@
   #   ];
   # };
 
+  # Bluetooth configuration
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
   # Steam
   programs.steam.enable = true;
 
