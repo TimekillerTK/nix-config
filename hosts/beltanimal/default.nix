9c7561190cf333ecb0d48d1424680a83d39761dd
TimekillerTK <erwartungen@protonmail.com>
2024-05-03 21:01:35 +0200

    (wip) enabled numlock on beltanimal for both login screen and astra user session

--- hosts/beltanimal/default.nix
+++ hosts/beltanimal/default.nix
@@ -65,6 +65,9 @@
     ];
   };
 
+  # Numlock on Login Screen (SDDM)
+  services.xserver.displayManager.setupCommands = ''${pkgs.numlockx}/bin/numlockx on'';
+
   # Bluetooth configuration
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
