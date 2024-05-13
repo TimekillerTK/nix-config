7fff66765732321965f59971423a42e25475f2f3
TimekillerTK <erwartungen@protonmail.com>
2024-05-13 11:01:23 +0200

    (wip) testing

--- hosts/caddy/default.nix
+++ hosts/caddy/default.nix
@@ -59,7 +59,7 @@
       respond "Hello, world on caddy.cyn.internal!"
     '';
     virtualHosts."spaghetti.cyn.internal".extraConfig = ''
-      reverse_proxy 192.168.112.1:8000
+      reverse_proxy whoami:8000
     '';
   };
 
