ce3a19f8a8ea3cde9145b021d16e05632c55acc2
TimekillerTK <erwartungen@protonmail.com>
2024-05-13 13:01:43 +0200

    (wip) new strategy

--- hosts/caddy/default.nix
+++ hosts/caddy/default.nix
@@ -59,13 +59,13 @@
       respond "Hello, world on caddy.cyn.internal!"
     '';
     virtualHosts."spaghetti.cyn.internal".extraConfig = ''
-      reverse_proxy 192.168.112.1:8000
+      reverse_proxy localhost:8010
     '';
     virtualHosts."spaghetti2.cyn.internal".extraConfig = ''
-      reverse_proxy localhost:8060
+      reverse_proxy localhost:8020
     '';
     virtualHosts."spaghetti3.cyn.internal".extraConfig = ''
-      reverse_proxy localhost:8070
+      reverse_proxy localhost:8030
     '';
   };
 
