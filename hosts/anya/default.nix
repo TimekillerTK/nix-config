910fc04a5d28ea046c16776b95d900e69e21d1fe
TimekillerTK <erwartungen@protonmail.com>
2024-09-06 17:01:30 +0200

    test

--- hosts/anya/default.nix
+++ hosts/anya/default.nix
@@ -7,8 +7,8 @@
   ...
 }: {
   imports = [
-    # Required for VS Code Remote
-    inputs.vscode-server.nixosModules.default
+    # # Required for VS Code Remote
+    # inputs.vscode-server.nixosModules.default
 
     # Required for disk configuration
     # inputs.disko.nixosModules.default
@@ -22,7 +22,7 @@
     # Repo Modules
     ../common/global
     ../common/users/tk
-    ../common/optional/sops
+    # ../common/optional/sops
     # ../common/optional/zfs
     ../common/optional/kde-plasma-x11
   ];
@@ -43,21 +43,21 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
-  # VS Code Server Module (for VS Code Remote)
-  services.vscode-server.enable = true;
+  # # VS Code Server Module (for VS Code Remote)
+  # services.vscode-server.enable = true;
 
-  # Actual SOPS keys
-  sops.secrets.smbcred = { };
-  sops.secrets.tailscale = { };
+  # # Actual SOPS keys
+  # sops.secrets.smbcred = { };
+  # sops.secrets.tailscale = { };
 
   # Adding CA root & intermediate certs
   security.pki.certificateFiles = [
     ../common/root-ca.pem
   ];
 
-  # Bluetooth configuration
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  # # Bluetooth configuration
+  # hardware.bluetooth.enable = true; # enables support for Bluetooth
+  # hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
   # Add printer autodiscovery
   services.avahi = {
@@ -79,10 +79,10 @@
   # Steam
   programs.steam.enable = true;
 
-  # Temporary
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-27.3.11"
-  ];
+  # # Temporary
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "electron-27.3.11"
+  # ];
 
   # For remapping HIDs
   # TODO: Needs to be started with sudo permissions automatically on login (?)
@@ -106,15 +106,15 @@
     kdePackages.kdialog # pops up dialogs
   ];
 
-  # Mounting fileshare
-  fileSystems."/mnt/FreeNAS" = {
-    device = "//freenas.cyn.internal/mediasnek2";
-    fsType = "cifs";
-    # TODO: UID should come from the user dynamically
-    # noauto + x-systemd.automount - disables mounting this FS with mount -a & lazily mounts (when first accessed)
-    # Remember to run `sudo umount /mnt/FreeNAS` before adding/removing "noauto" + "x-systemd.automount"
-    options = [ "credentials=/run/secrets/smbcred" "noserverino" "rw" "_netdev" "uid=1000"] ++ ["noauto" "x-systemd.automount"];
-  };
+  # # Mounting fileshare
+  # fileSystems."/mnt/FreeNAS" = {
+  #   device = "//freenas.cyn.internal/mediasnek2";
+  #   fsType = "cifs";
+  #   # TODO: UID should come from the user dynamically
+  #   # noauto + x-systemd.automount - disables mounting this FS with mount -a & lazily mounts (when first accessed)
+  #   # Remember to run `sudo umount /mnt/FreeNAS` before adding/removing "noauto" + "x-systemd.automount"
+  #   options = [ "credentials=/run/secrets/smbcred" "noserverino" "rw" "_netdev" "uid=1000"] ++ ["noauto" "x-systemd.automount"];
+  # };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
