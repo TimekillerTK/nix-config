a7452947a278f51c06117ebe24d900f6859a89dd
TimekillerTK <erwartungen@protonmail.com>
2024-09-06 17:21:44 +0200

    test

--- hosts/common/global/nix_settings.nix
+++ hosts/common/global/nix_settings.nix
@@ -1,10 +1,6 @@
 # Common Nix Settings config
-{ config, lib, inputs, ... }: 
+{ config, lib, ... }:
 {
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
   nix.nixPath = ["/etc/nix/path"];
