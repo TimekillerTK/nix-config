69a01768633abdc343899c41be5599898f68039e
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2024-09-17 21:06:21 +0200

    (wip) disable everything HT

--- hosts/hummingbird/hardware-configuration.nix
+++ hosts/hummingbird/hardware-configuration.nix
@@ -16,7 +16,7 @@
   # Temporary disable of ACPI to manage these kinds of errors:
   # [    7.204249] ACPI Error: Aborting method \_SB.PCI0.GPP2.PTXH.RHUB.PO11._PLD due to previous error (AE_AML_UNINITIALIZED_ELEMENT) (20230628/psparse-529)
   # boot.kernelParams = [ "acpi=off" ]; # this causes display problems
-  boot.kernelParams = [ "acpi=strict" ];
+  boot.kernelParams = [ "acpi=ht" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
