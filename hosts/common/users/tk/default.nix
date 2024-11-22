835951f22b776bf3330a6e84c38839ad47e2f33d
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2024-11-22 15:51:37 +0100

    (wip) Moved pubkeys to a common directory; Added hummingbird

--- hosts/common/users/tk/default.nix
+++ hosts/common/users/tk/default.nix
@@ -1,17 +1,20 @@
-{ pkgs, lib, ... }:
 {
+  pkgs,
+  lib,
+  ...
+}: {
   # Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     tk = {
       isNormalUser = true;
       shell = pkgs.zsh;
       initialPassword = "Hello123!";
-      extraGroups = [ "networkmanager" "wheel" ];
+      extraGroups = ["networkmanager" "wheel"];
       openssh.authorizedKeys.keys = [
-        (builtins.readFile ./anya.pub)
-        (builtins.readFile ./beltanimal.pub)
-        (builtins.readFile ./dockerhost.pub)
-        (builtins.readFile ./mbp.pub)
+        (builtins.readFile ../../../lib/ssh_keys/anya.pub)
+        (builtins.readFile ../../../lib/ssh_keys/beltanimal.pub)
+        (builtins.readFile ../../../lib/ssh_keys/mbp.pub)
+        (builtins.readFile ../../../lib/ssh_keys/hummingbird.pub)
       ];
     };
   };
