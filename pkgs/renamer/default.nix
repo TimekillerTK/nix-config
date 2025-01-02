0c76efe6e835a58bc32ef289108f2d5948dcd237
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-01-02 20:49:23 +0100

    (wip) bumped renamer version

--- pkgs/renamer/default.nix
+++ pkgs/renamer/default.nix
@@ -2,19 +2,19 @@
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "renamer";
-  version = "0.1.0";
+  version = "0.1.1";
 
   # cargoLock.lockFile = "./Cargo.lock"; # <- when local
   src = pkgs.fetchFromGitHub {
     owner = "TimekillerTK";
     repo = "renamer";
     rev = "main";
-    sha256 = "sha256-8N3IOyLwz1gewrXtpjP2puHHCcaGRcLpPd2nRuUoKPk=";
+    sha256 = "sha256-FtxI7umh2VMsZqPCOQWqVGO2QbRnQbdD480NMFdYOms=";
   };
 
   # Required for building on MacOS
   buildInputs = [] ++ lib.optional stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
 
   # cargoHash = lib.fakeHash; # <- generate fake hash
-  cargoHash = "sha256-vuczX9KLgnToiZlO3b1bCGV/TJwVQAt8oAL8mAeUKbo=";
+  cargoHash = "sha256-GCr+ACAVGsv0DDuluAwLHYxtGLmQ+dzv8nDGUrtcNXw=";
 }
