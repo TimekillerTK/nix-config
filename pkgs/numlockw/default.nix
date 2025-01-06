a84dab77fe12e057d389608d42aaa3a4c0bd06c5
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-01-06 11:01:38 +0100

    (wip) trying pypi install method for numlockw instead

--- pkgs/numlockw/default.nix
+++ pkgs/numlockw/default.nix
@@ -1,21 +1,34 @@
 {
   pkgs,
   pythonPkgs ? pkgs.python3Packages,
+  fetchPypi,
 }:
-pythonPkgs.buildPythonApplication {
+pythonPkgs.buildPythonApplication rec {
   pname = "numlockw";
   version = "0.1.2";
 
-  # cargoLock.lockFile = "./Cargo.lock"; # <- when local
-  src = pkgs.fetchFromGitHub {
-    owner = "xz-dev";
-    repo = "numlockw";
-    rev = "1800a5072323bbc026c80bc737285d8ea363af78";
-    sha256 = "sha256-Z4ymxTw+OLh1k2Ysh3HQ3IbgJ+GkDb4EoR4lQjFBM48=";
+  # src = pkgs.fetchFromGitHub {
+  #   owner = "xz-dev";
+  #   repo = "numlockw";
+  #   rev = "1800a5072323bbc026c80bc737285d8ea363af78";
+  #   sha256 = "sha256-Z4ymxTw+OLh1k2Ysh3HQ3IbgJ+GkDb4EoR4lQjFBM48=";
+  # };
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-b5/x9qC98Upd8z56ovTHi36DCAEz+PtNOjJTHnAfZcU=";
   };
+
+  # If your app doesn't have tests, you can disable them
+  doCheck = false;
 
   propagatedBuildInputs = with pythonPkgs; [
     # List your Python dependencies here
     evdev
   ];
+
+  # installPhase = ''
+  #   mkdir -p $out/bin
+  #   cp your_main_script.py $out/bin/your-application
+  #   chmod +x $out/bin/your-application
+  # '';
 }
