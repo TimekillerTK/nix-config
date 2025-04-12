53aa501f2aea9bfa352b483b48b4c40c0ca65efb
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-04-12 14:18:29 +0200

    (fixed) Now tailscale clients actually won't start on boot, unlike previous commit

--- hosts/common/optional/tailscale-client/default.nix
+++ hosts/common/optional/tailscale-client/default.nix
@@ -2,9 +2,13 @@
 {
   sops.secrets.tailscale = { };
 
+  # Stop tailscale starting on boot
+  # -> to start run `sudo systemctl start tailscaled`
+  systemd.services.tailscaled.enable = false;
+
   # Tailscale
   services.tailscale = {
-    enable = false; # Disable tailscale from starting on boot
+    enable = true;
     authKeyFile = "/run/secrets/tailscale";
     extraUpFlags = [
       "--advertise-tags=tag:usermachine"
