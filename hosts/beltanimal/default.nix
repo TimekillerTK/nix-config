010b683f75a408cc702db54f9b3a7d98c7d01f79
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-04-22 09:56:26 +0200

    (working) added homeassistant user/group; added sudo config for the homeassitant user to perform shutdown/reboot/poweroff;

--- hosts/beltanimal/default.nix
+++ hosts/beltanimal/default.nix
@@ -122,6 +122,32 @@
     lidSwitchExternalPower = "lock";
   };
 
+  # homeassistant user for shutdown via SSH command
+  users = {
+    groups = { homeassistant = {}; }; # group for homeassistant user (required)
+    users = {
+      homeassistant = {
+        shell = pkgs.zsh;
+        isSystemUser = true;
+        group = "homeassistant";
+        openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbyRIFCnKqR6DXV2vJLd9s8JRjnvwyKJWw8VevEzfSC"];
+      };
+    };
+  };
+
+  # Passwordless sudo for specific binaries for the homeassistant user
+  # to allow user to shutdown/reboot/poweroff the machine
+  security.sudo.extraRules = [
+    {
+      users = ["homeassistant"];
+      commands = [
+        { command = "/run/current-system/sw/bin/shutdown"; options = ["NOPASSWD"]; }
+        { command = "/run/current-system/sw/bin/reboot"; options = ["NOPASSWD"]; }
+        { command = "/run/current-system/sw/bin/poweroff"; options = ["NOPASSWD"]; }
+      ];
+    }
+  ];
+
   # Hostname & Network Manager
   networking.hostName = "beltanimal";
   networking.networkmanager = {
