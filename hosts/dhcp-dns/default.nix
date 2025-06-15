3d98f202a05989f3002fe6b6e11639d12086b88e
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-06-15 15:35:44 +0200

    wip

--- hosts/dhcp-dns/default.nix
+++ hosts/dhcp-dns/default.nix
@@ -61,7 +61,13 @@ in {
   # Hostname & Network Manager
   networking = {
     hostName = "dhcp-dns";
+
+    # Disable existing IPTables firewall & NAT
+    nat.enable = false;
     firewall.enable = false; # Using nftables (SOON)
+
+    # NOTE: will be explictly configured per interface if needed
+    useDHCP = false;
 
     vlans = {
       home = {
@@ -81,12 +87,12 @@ in {
     interfaces = {
       # Physical NICs
       ens18 = {
-        # name = wanPort;
+        name = wanPort;
         macAddress = wanMacAddress;
         useDHCP = true;
       };
       ens19 = {
-        # name = lanPort;
+        name = lanPort;
         macAddress = lanMacAddress;
         useDHCP = false;
         ipv4.addresses = [{
@@ -115,6 +121,104 @@ in {
         }];
       };
     };
+
+    # nftables = {
+    #   # Enable NFTables & flush any existing rulesets
+    #   enable = true;
+    #   flushRuleset = true;
+
+    #   # Actual NFTables rules
+    #   ruleset = ''
+    #     # Source: https://oxcrag.net/projects/linux-router-part-1-routing-nat-and-nftables/
+    #     # Our future selves will thank us for noting what cable goes where and labeling the relevant network interfaces if it isn't already done out-of-the-box.
+    #     define WANPORT = ${wanPort}
+    #     define LANPORT = ${lanPort}
+
+    #     # We never expect to see the following address ranges on the Internet
+    #     define BOGONS4 = {
+    #       0.0.0.0/8,
+    #       10.0.0.0/8,
+    #       10.64.0.0/10,
+    #       127.0.0.0/8,
+    #       127.0.53.53,
+    #       169.254.0.0/16,
+    #       172.16.0.0/12,
+    #       192.0.0.0/24,
+    #       192.0.2.0/24,
+    #       192.168.0.0/16,
+    #       198.18.0.0/15,
+    #       198.51.100.0/24,
+    #       203.0.113.0/24,
+    #       224.0.0.0/4,
+    #       240.0.0.0/4,
+    #       255.255.255.255/32
+    #     }
+
+    #     table inet filter {
+    #       chain inbound_world {
+    #         # Drop obviously spoofed inbound traffic (to turn on later)
+    #         # ip saddr { $BOGONS4 } drop
+    #       }
+
+    #       chain inbound_private {
+    #         # We want to allow remote access over ssh, incoming DNS traffic, and incoming DHCP traffic
+	  #       	ip protocol . th dport vmap { tcp . 22 : accept, udp . 53 : accept, tcp . 53 : accept, udp . 67 : accept }
+	  #       }
+
+    #       chain inbound {
+    #         # Default Deny
+    #         type filter hook input priority 0; policy drop;
+
+    #         # Allow established and related connections: Allows Internet servers to respond to requests from our Internal network
+    #         ct state vmap { established : accept, related : accept, invalid : drop} counter
+
+    #         # ICMP is - mostly - our friend. Limit incoming pings somewhat, but allow necessary information.
+    #         icmp type echo-request counter limit rate 5/second accept
+    #         ip protocol icmp icmp type { destination-unreachable, echo-reply, echo-request, source-quench, time-exceeded } accept
+
+    #         # Drop obviously spoofed loopback traffic
+    #         iifname "lo" ip daddr != 127.0.0.0/8 drop
+
+    #         # Separate rules for traffic from Internet and from the internal network
+    #         iifname vmap { lo: accept, $WANPORT : jump inbound_world, $LANPORT : jump inbound_private }
+    #       }
+
+    #       # Rules for sending traffic from one network interface to another
+    #       chain forward {
+    #         # Default deny, again
+    #         type filter hook forward priority 0; policy drop;
+
+    #         # Accept established and related traffic
+    #         ct state vmap { established : accept, related : accept, invalid : drop }
+
+    #         # Let traffic from this router and from the Internal network get out onto the Internet
+    #         iifname { lo, $LANPORT } accept
+    #       }
+    #     }
+
+    #     # Network address translation: What allows us to glue together a private network with the Internet even though we only have one routable address, as per IPv4 limitations
+    #     table ip nat {
+    #       chain prerouting {
+    #         type nat hook prerouting priority dstnat; policy accept;
+
+    #         # Redirect all DNS traffic destined to google DNS to local DNS server
+    #         iifname $LANPORT ip daddr 8.8.8.8 udp dport 53 counter ct mark set 1 dnat to 172.17.0.40:53
+    #         iifname $LANPORT ip daddr 8.8.8.8 tcp dport 53 counter ct mark set 1 dnat to 172.17.0.40:53
+    #         iifname $LANPORT ip daddr 8.8.4.4 udp dport 53 counter ct mark set 1 dnat to 172.17.0.40:53
+    #         iifname $LANPORT ip daddr 8.8.4.4 tcp dport 53 counter ct mark set 1 dnat to 172.17.0.40:53
+    #       }
+    #       chain postrouting {
+    #         type nat hook postrouting priority 100; policy accept;
+
+    #         # Pretend that redirected DNS requests originate in this router, so clients can get a valid response
+    #         ct mark 1 counter masquerade
+
+    #         # Pretend that outbound traffic originates in this router so that Internet servers know where to send responses
+    #         oifname $WANPORT masquerade
+    #       }
+    #     }
+    #   '';
+    # };
   };
 
   # Kea DHCP config
