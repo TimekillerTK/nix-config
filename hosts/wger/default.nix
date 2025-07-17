6fc11d45486ec1345a8d91b0023b315706b79b47
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-07-17 10:05:50 +0200

    wip

--- hosts/wger/default.nix
+++ hosts/wger/default.nix
@@ -36,6 +36,7 @@
 
   # Required for our user
   users.users.tk.shell = lib.mkForce pkgs.bash;
+  users.users.tk.extraGroups = lib.mkForce [ "networkmanager" "wheel" "docker" ];
 
   # Hostname & Network Manager
   networking.hostName = "wger";
