f4bb9063ec6bb2c1114719856c3d873f0ba1dc22
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-07-22 08:28:39 +0200

    wip

--- hosts/jellyfin/default.nix
+++ hosts/jellyfin/default.nix
@@ -47,6 +47,19 @@
     ../common/root-ca.pem
   ];
 
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD"; # Or "i965" if using older driver
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };      # Same here
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+      libva-vdpau-driver # Previously vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-ocl # OpenCL support
+    ];
+  };
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
