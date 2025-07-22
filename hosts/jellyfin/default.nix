34da1bc47bd659bd5678b8fa5afa589c77d3463d
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-07-22 10:54:32 +0200

    wip

--- hosts/jellyfin/default.nix
+++ hosts/jellyfin/default.nix
@@ -39,8 +39,8 @@
   users.users.tk.extraGroups = lib.mkForce [
     "networkmanager"
     "wheel"
-    "video"
-    "render"
+    "video" # needed for vainfo
+    "render" # needed for vainfo
   ];
 
   # Hostname & Network Manager
@@ -52,7 +52,6 @@
     ../common/root-ca.pem
   ];
 
-  boot.kernelParams = [ "i915.force_probe=46d4" ];
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
@@ -60,15 +59,21 @@
       libva-vdpau-driver # Previously vaapiVdpau
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       vpl-gpu-rt # QSV on 11th gen or newer
+      intel-gpu-tools # intel_gpu_top (for checking)
     ];
   };
 
+  # Set the VA-API driver environment variable.
   environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
   systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-  };
+
+  # Includes firmware blobs for the i915 driver
+  hardware.enableRedistributableFirmware = true;
+
+  # Correct kernel parameters for the i915 driver.
+  # - i915.enable_guc=2 enables both the GuC (for scheduling) and HuC (for media
+  #   decode/encode), which is required for hardware transcoding.
+  boot.kernelParams = [ "i915.enable_guc=2" ];
 
   # Needed for Transcoding
   users.users.jellyfin.extraGroups = ["video" "render"];
@@ -77,6 +82,12 @@
     jellyfin-ffmpeg
     libva-utils # vainfo
   ];
+
+  # Jellyfin config
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
