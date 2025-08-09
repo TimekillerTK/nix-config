93c4a2b1c8eb37c6e89f620dfbbf55c788797b34
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-08-09 09:18:42 +0200

    (wip) newer kernel for dockerhost

--- hosts/dockerhost/default.nix
+++ hosts/dockerhost/default.nix
@@ -37,7 +37,7 @@ in
   sops.secrets.smbcred = { };
 
   # Newer LTS Kernel, pinned
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_6_15;
 
   # use default bash
   # TODO: find a better way to do this
