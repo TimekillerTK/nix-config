bf714882f2e70ef8957b115d52cea28fc071405b
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-10-12 11:11:00 +0200

    wip

--- hosts/common/optional/desktop-auto-update/check-git-update.sh
+++ hosts/common/optional/desktop-auto-update/check-git-update.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+# Acquire the PID of the active plasmashell
+TARGET_PID=$(ps -u "tk" -f | rg plasmashell | head -n 1 | awk '{print $2}')
+
+# Get & set needed environment variables
+eval "$(tr '\0' '\n' <"/proc/${TARGET_PID}/environ" | rg 'XDG_RUNTIME_DIR|WAYLAND_DISPLAY|DBUS_SESSION_BUS_ADDRESS')"
+
+# If ANY env var not set, exit with an error!
+if [ -z "$XDG_RUNTIME_DIR" ] || [ -z "$WAYLAND_DISPLAY" ] || [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+	echo "Required environment variables not set!"
+	exit 1
+fi
+
 REPO="/nix-config"
 cd "$REPO" && git fetch
 
@@ -6,10 +19,13 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse '@{u}')
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-	# Notify-send works on KDE with libnotify under Wayland
+	# notify-send works on KDE with libnotify under Wayland
+	echo "Update Available - There are new updates on the remote repository."
+
+	ls -la
+
 	notify-send "Update Available" "There are new updates on the remote repository."
 else
 	echo "No updates, nothing to do."
 fi
 
-TARGET_PID=$($TOOL_PS -u "$username" -f | $TOOL_RG plasmashell | head -n 1 | $TOOL_AWK '{print $2}')
