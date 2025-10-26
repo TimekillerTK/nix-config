4e0188dcc3ea670da368f23e7b27319051d098e0
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-10-26 11:18:28 +0100

    (wip) Testing nix-auto-update

--- pkgs/nix-auto-update/default.nix
+++ pkgs/nix-auto-update/default.nix
@@ -11,7 +11,7 @@ pkgs.rustPlatform.buildRustPackage {
     owner = "TimekillerTK";
     repo = "nix-auto-update";
     rev = "dev";
-    sha256 = "sha256-6FlwlcJCG+SYBceI0GoSnEBukF/oIkZKrWyh5Xi3sFo=";
+    sha256 = "sha256-mCm87u36gpxP8PUTf9cW7uQVXG0M3vgNhmVQHmeD4Qw=";
   };
 
   # Required for building the binary
