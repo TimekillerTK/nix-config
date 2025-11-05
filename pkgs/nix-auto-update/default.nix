d716ed7aad294ecf61cc4d77fa68b91c1af6cf34
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-11-05 19:13:34 +0100

    (wip) nix-auto-update version 0.1.7

--- pkgs/nix-auto-update/default.nix
+++ pkgs/nix-auto-update/default.nix
@@ -5,13 +5,13 @@
 }:
 pkgs.rustPlatform.buildRustPackage {
   pname = "nix-auto-update";
-  version = "0.1.6";
+  version = "0.1.7";
 
   src = pkgs.fetchFromGitLab {
     owner = "TimekillerTK";
     repo = "nix-auto-update";
-    rev = "959dfecc53eeced1de4d6ecb5a71e02721781ae5";
-    sha256 = "sha256-liYprnTP5G6N6ggskHFEg/7b7QWtJxzSwe9o9iEu+mU=";
+    rev = "d4ebcc793fdc8b83a034b78709230b2ca69737e8";
+    sha256 = "sha256-LbGpTBaX5JeGQUjmTYsxiMnl4HdNgVd3uY4R3QpeT5s=";
   };
 
   # Required for building the binary
@@ -27,5 +27,5 @@ pkgs.rustPlatform.buildRustPackage {
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
 
-  cargoHash = "sha256-JnBz8A3ueKMTCHAkUrwj6tYDwYqBSz5H/MFXCv89NIk=";
+  cargoHash = "sha256-HNJvW1oN+m0t2LIzlUHNeQp/TQ5e17uOY2tY3vcLe0s=";
 }
