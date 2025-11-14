2b10b0db8355ed88cb5968e21ad33891acbf7a78
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2025-11-14 16:52:54 +0100

    wip

--- hosts/eyeball/default.nix
+++ hosts/eyeball/default.nix
@@ -123,7 +123,7 @@
 
   # For accessing the WebUI remotely
   # TODO: Better way?
-  networking.firewall.allowedTCPPorts = [80 3000 9090];
+  networking.firewall.allowedTCPPorts = [80 3000 9090 9000];
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
