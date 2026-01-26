4b845fdeb01cff6466a2ee089c403a8abf4a108f
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-01-26 06:52:25 +0100

    (wip) removed vscode-server from ca

--- hosts/ca/default.nix
+++ hosts/ca/default.nix
@@ -36,9 +36,6 @@
   users.users.tk.shell = lib.mkForce pkgs.bash;
   users.users.tk.extraGroups = lib.mkForce ["networkmanager" "wheel" "docker"];
 
-  # VS Code Server Module (for VS Code Remote)
-  services.vscode-server.enable = true;
-
   # Hostname & Network Manager
   networking.hostName = "ca";
   networking.networkmanager.enable = true;
