0dd0b82b51a921892501f87e61e85bef615afdb8
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-02 06:44:19 +0100

    wip

--- hosts/dendritic/default.nix
+++ hosts/dendritic/default.nix
@@ -13,7 +13,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "dendritic"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
