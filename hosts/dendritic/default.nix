5e6b6cfd45283943d16aded3649043c7822975fe
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-02 06:46:23 +0100

    m

--- hosts/dendritic/default.nix
+++ hosts/dendritic/default.nix
@@ -8,11 +8,6 @@
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
   networking.hostName = "dendritic"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
