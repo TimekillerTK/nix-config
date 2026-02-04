44ccd194d3648caf4165e77071fb983fbb7316e2
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-04 07:09:59 +0100

    Split HW into a different module

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
       flake.nixosConfigurations.example = inputs.nixpkgs.lib.nixosSystem {
         modules = [
           self.nixosModules.example
+          self.nixosModules.examplehw
         ];
       };
 
@@ -90,6 +91,36 @@
 
         system.stateVersion = "25.05";
 
+        # # hardware-configuration.nix --------
+        # imports = [
+        #   (modulesPath + "/profiles/qemu-guest.nix")
+        # ];
+        # boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
+        # boot.initrd.kernelModules = [];
+        # boot.kernelModules = [];
+        # boot.extraModulePackages = [];
+
+        # boot.loader.grub = {
+        #   devices = ["/dev/sda"];
+        # };
+
+        # fileSystems."/" = {
+        #   device = "/dev/disk/by-uuid/0119ab10-0458-4582-bb94-2a67176abcf2";
+        #   fsType = "ext4";
+        # };
+
+        # swapDevices = [
+        #   {device = "/dev/disk/by-uuid/cdf37e71-63a7-473e-9047-ba08b706f6ef";}
+        # ];
+        # networking.useDHCP = inputs.nixpkgs.lib.mkDefault true;
+        # nixpkgs.hostPlatform = inputs.nixpkgs.lib.mkDefault "x86_64-linux";
+      };
+
+      flake.nixosModules.examplehw = {
+        pkgs,
+        modulesPath,
+        ...
+      }: {
         # hardware-configuration.nix --------
         imports = [
           (modulesPath + "/profiles/qemu-guest.nix")
