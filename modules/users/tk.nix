544ce78df82517bb038dd4bfd6512557d5652076
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-11 21:42:10 +0100

    wip - users

---
+++ modules/users/tk.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.nixos.tk = {pkgs, ...}: {
+    # Configure your system-wide user settings (groups, etc), add more users as needed.
+    users.users = {
+      tk = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        initialPassword = "Hello123!";
+        extraGroups = ["networkmanager" "wheel"];
+        openssh.authorizedKeys.keys = [
+          (builtins.readFile ../../../../lib/ssh_keys/anya.pub)
+          (builtins.readFile ../../../../lib/ssh_keys/beltanimal.pub)
+          (builtins.readFile ../../../../lib/ssh_keys/mbp.pub)
+          (builtins.readFile ../../../../lib/ssh_keys/hummingbird.pub)
+        ];
+      };
+    };
+
+    # Passwordless Sudo
+    security.sudo.extraRules = [
+      {
+        users = ["tk"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
+}
