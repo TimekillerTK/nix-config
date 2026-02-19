78e7c78df164a88a0b7cee198e3120d09e1e27d9
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-19 16:54:29 +0100

    added proper bb to anya

--- modules/users/bb.nix
+++ modules/users/bb.nix
@@ -1,19 +1,31 @@
 {
-  flake.modules.nixos.bb = {pkgs, ...}: {
-    # Configure your system-wide user settings (groups, etc), add more users as needed.
-    users.users = {
-      bb = {
-        isNormalUser = true;
-        shell = pkgs.zsh;
-        initialPassword = "Hello123!";
-        extraGroups = ["networkmanager" "wheel"];
-        openssh.authorizedKeys.keys = [
-          (builtins.readFile ../../ssh_keys/anya.pub)
-          (builtins.readFile ../../ssh_keys/beltanimal.pub)
-          (builtins.readFile ../../ssh_keys/mbp.pub)
-          (builtins.readFile ../../ssh_keys/hummingbird.pub)
+  inputs,
+  self,
+  lib,
+  ...
+}: {
+  flake.modules = lib.mkMerge [
+    (self.factory.user "bb" true)
+    {
+      nixos.bb = {
+        # Configure your system-wide user settings (groups, etc), add more users as needed.
+        users.users = {
+          bb = {
+            extraGroups = ["networkmanager"];
+            openssh.authorizedKeys.keys = [
+              (builtins.readFile ../../ssh_keys/anya.pub)
+              (builtins.readFile ../../ssh_keys/beltanimal.pub)
+              (builtins.readFile ../../ssh_keys/mbp.pub)
+              (builtins.readFile ../../ssh_keys/hummingbird.pub)
+            ];
+          };
+        };
+      };
+      homeManager.bb = {
+        imports = [
+          inputs.self.modules.homeManager.system-desktop
         ];
       };
-    };
-  };
+    }
+  ];
 }
