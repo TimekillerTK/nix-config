19039e0fa7ab5ecfc9da26b0fa110036f748ecd1
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-02-26 17:16:01 +0100

    wip

--- modules/hosts/example/_disko.nix
+++ modules/hosts/example/_disko.nix
@@ -8,7 +8,7 @@
           type = "gpt";
           partitions = {
             ESP = {
-              size = "1G"; # Recommended @ ArchWiki
+              size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
@@ -22,11 +22,11 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
-                # mountOptions = ["noatime" "data=ordered"];
               };
             };
             swap = {
               size = "1G";
+              type = "8200";
               content = {
                 type = "swap";
               };
@@ -35,65 +35,5 @@
         };
       };
     };
-    # zpool = {
-    #   zroot = {
-    #     type = "zpool";
-    #     rootFsOptions = {
-    #       # ZFS Tuning Options
-    #       # https://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/
-    #       # https://openzfs.github.io/openzfs-docs/Basic%20Concepts/Checksums.html
-    #       # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#
-    #       xattr = "sa"; # set Extended Attributes directly in Inodes (+perf)
-    #       dnodesize = "auto"; # recommended with xattr = "sa"
-    #       compression = "lz4"; # (+perf & +space)
-    #       atime = "off"; # disables last file/directory access time updates
-    #       acltype = "posixacl"; # support for POSIX ACLs
-    #       canmount = "off"; # don't mount the pool by default, mount datasets instead
-    #       checksum = "edonr"; # most performant checksum for zfs
-    #       normalization = "formD"; # recommended default
-    #     };
-
-    #     mountpoint = null;
-    #     options = {
-    #       ashift = "12"; # 4096 sector size (recommended)
-    #       autotrim = "on";
-    #     };
-
-    #     datasets = {
-    #       local = {
-    #         type = "zfs_fs";
-    #         options.canmount = "off";
-    #       };
-
-    #       "local/root" = {
-    #         type = "zfs_fs";
-    #         mountpoint = "/";
-    #         options.mountpoint = "/";
-    #         options."com.sun:auto-snapshot" = "true";
-    #       };
-
-    #       "local/home" = {
-    #         type = "zfs_fs";
-    #         mountpoint = "/home";
-    #         options.mountpoint = "/home";
-    #         options."com.sun:auto-snapshot" = "true";
-    #       };
-
-    #       "local/nix" = {
-    #         type = "zfs_fs";
-    #         mountpoint = "/nix";
-    #         options.mountpoint = "/nix";
-    #         options."com.sun:auto-snapshot" = "false";
-    #       };
-
-    #       "local/data" = {
-    #         type = "zfs_fs";
-    #         mountpoint = "/data";
-    #         options.mountpoint = "/data";
-    #         options."com.sun:auto-snapshot" = "false";
-    #       };
-    #     };
-    # };
-    # };
   };
 }
