60cec6dec1dce82c1d57012a0ee0fbbb4bb5b314
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-05 08:52:39 +0100

    wip

---
+++ modules/features/helix.nix
@@ -0,0 +1,35 @@
+# Helix Home-Manager IDE Config
+{
+  flake.modules.homeManager.helix = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      unstable.helix # Code Editor
+
+      # Language Servers (and/or similar)
+      vscode-langservers-extracted # support for many others
+      yaml-language-server
+      unstable.nixd # LSP
+      pyright # Python
+      taplo # TOML
+      bash-language-server # bash
+      shfmt # shell/bash formatter
+      alejandra # Nix Formatter
+      nodePackages.prettier # YAML formatter
+      typescript-language-server # TypeScript
+      wl-clipboard # copy/paste support from system clipboard on Wayland
+    ];
+
+    # To stop myself from automatically using `code .`
+    programs.zsh.shellAliases = {
+      code = "${pkgs.unstable.helix}/bin/hx";
+    };
+
+    # Set Helix as the default editor (for git operations and such)
+    home.sessionVariables.EDITOR = "hx";
+
+    home.file = {
+      # Helix IDE config files
+      ".config/helix/config.toml".source = ../../dotfiles/helix/config.toml;
+      ".config/helix/languages.toml".source = ../../dotfiles/helix/languages.toml;
+    };
+  };
+}
