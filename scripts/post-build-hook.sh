48b03b660ee3edbfac4192da9ef71377c2592b0d
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-19 11:19:17 +0100

    (wip) dropped the first exec, because it prevents the second from running entirely

--- scripts/post-build-hook.sh
+++ scripts/post-build-hook.sh
@@ -5,7 +5,7 @@ set -f # disable globbing
 export IFS=' '
 
 echo "Signing paths" $OUT_PATHS
-exec nix store sign --key-file /home/tk/DELETE_ME $OUT_PATHS
+nix store sign --key-file /home/tk/DELETE_ME $OUT_PATHS
 
 echo "Uploading paths" $OUT_PATHS
 exec nix copy --to "ssh-ng://tk@host.nix-cache.cyn.internal?ssh-key=/home/tk/.ssh/id_ed25519" $OUT_PATHS
