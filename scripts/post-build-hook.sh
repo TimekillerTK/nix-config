7e68f095fa95b570a4f7ebd229ab582c225ba60c
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-19 11:06:07 +0100

    (wip) testing out post build hook

---
+++ scripts/post-build-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -eu
+set -f # disable globbing
+export IFS=' '
+
+echo "Signing paths" $OUT_PATHS
+exec nix store sign --key-file /home/tk/DELETE_ME $OUT_PATHS
+
+echo "Uploading paths" $OUT_PATHS
+exec nix copy --to "ssh-ng://tk@host.nix-cache.cyn.internal?ssh-key=/home/tk/.ssh/id_ed25519" $OUT_PATHS
