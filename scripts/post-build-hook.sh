881b9174882b4ec83bb8694ce0e9079a5cfa8108
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-20 11:03:12 +0100

    Skip uploading to cache if can't be reached

--- scripts/post-build-hook.sh
+++ scripts/post-build-hook.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 # NOTE: Source for this is https://nix.dev/manual/nix/2.33/advanced-topics/post-build-hook.html
-
+#
+# NOTE: To skip this build hook during a nixos rebuld, an option can be passed with the
+# rebuild command:
+#
+#    sudo nixos-rebuild switch --flake .# --option post-build-hook ""
+#
 set -eu
 set -f # disable globbing
 export IFS=' '
+
+CACHE_HOST="host.nix-cache.cyn.internal"
 
 # We need to sign whatever we are building with a key our nix binary cache trusts, in
 # my case it's harmonia's secret
@@ -14,8 +21,9 @@ nix store sign --key-file /var/lib/secrets/harmonia.secret "$OUT_PATHS"
 # NOTE: Using the ssh key of my user since it already is configured, but should ideally
 # be it's own dedicated ssh key and user with limited permissions
 #
-# TODO add condition to check:
-#    ping -c 1 host.nix-cache.cyn.internal > /dev/null 2&>1
-#
-echo "Uploading paths" "$OUT_PATHS"
-exec nix copy --to "ssh-ng://tk@host.nix-cache.cyn.internal?ssh-key=/home/tk/.ssh/id_ed25519" "$OUT_PATHS"
+if ping -c 1 $CACHE_HOST > /dev/null 2&>1; then
+  echo "Uploading paths" "$OUT_PATHS"
+  exec nix copy --to "ssh-ng://tk@$CACHE_HOST?ssh-key=/home/tk/.ssh/id_ed25519" "$OUT_PATHS"
+else
+  echo "Ping to $CACHE_HOST failed, skipping upload." >&2
+fi
