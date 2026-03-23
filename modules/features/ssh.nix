4def6985590737a72e15f9ff69a244737ac486bb
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-23 13:43:00 +0100

    (wip) added anya (remote builder) to known hosts in ssh config

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -3,6 +3,12 @@
     # SSH Config
     services.openssh = {
       enable = true;
+      knownHosts = {
+        "anya" = {
+          hostNames = ["anya" "anya.cyn.internal"];
+          publicKey = "anya.cyn.internal ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiYlYEljfXg8k5TzGkmaIyVGBS08ecklxGoozhLMpMj";
+        };
+      };
       settings = {
         PermitRootLogin = "no";
         PasswordAuthentication = lib.mkDefault false;
