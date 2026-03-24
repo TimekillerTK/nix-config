a21f2946a9d15f559e8066df94279fa4e121741e
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-24 10:50:45 +0100

    wip

--- modules/hosts/anya/configuration.nix
+++ modules/hosts/anya/configuration.nix
@@ -117,13 +117,13 @@
         ];
       }
     ];
-    services.openssh.extraConfig = ''
-      Host host.nix-cache.cyn.internal
-        User tk
-        IdentityFile /var/lib/secrets/id_ed25519-nix-cache
-        IdentitiesOnly yes
-        Port 22
-    '';
+    # services.openssh.extraConfig = ''
+    #   Host host.nix-cache.cyn.internal
+    #     User tk
+    #     IdentityFile /var/lib/secrets/id_ed25519-nix-cache
+    #     IdentitiesOnly yes
+    #     Port 22
+    # '';
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
