6e205232b4f26ce6359b68235251587e686fbb5f
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-03-24 13:52:01 +0100

    wip

--- modules/hosts/anya/configuration.nix
+++ modules/hosts/anya/configuration.nix
@@ -163,7 +163,6 @@
     # System Packages
     environment.systemPackages = [
       pkgs.kdePackages.kdialog # pops up dialogs
-      pkgs.zmate
     ];
 
     # Generated with head -c4 /dev/urandom | od -A none -t x4
