acd597db4ec1caedf75a1bf2ace3bc2b7e2ef5ce
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-05-24 11:15:44 +0200

    whoopsied jellyfin module

--- modules/hosts/jellyfin/configuration.nix
+++ modules/hosts/jellyfin/configuration.nix
@@ -1,7 +1,7 @@
 {inputs, ...}: {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "jellyfin";
 
-  flake.modules.nixos.anya = {
+  flake.modules.nixos.jellyfin = {
     pkgs,
     config,
     ...
