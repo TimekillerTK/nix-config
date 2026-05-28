7b0631c8b870bb3f2b19458e38c81277b8f4892a
TimekillerTK <8354689-TimekillerTK@users.noreply.gitlab.com>
2026-05-28 08:51:24 +0200

    (wip) moved zfs hosts to kernel version 7.0; pinned to 7.0.6 due to bluetooth bug; nix flake update

--- modules/features/zfs.nix
+++ modules/features/zfs.nix
@@ -3,19 +3,26 @@
     lib,
     pkgs,
     ...
-  }: {
+  }: let
+    # NOTE: Temporarily pinned since nixpkgs has version 7.0.9 which has a broken
+    # mediatek bluetooth driver
+    # https://community.frame.work/t/bluetooth-is-borked-cant-init-device-hci0-invalid-argument-22/82605/6
+    linux_7_0_6 = pkgs.linux_7_0.override {
+      argsOverride = rec {
+        version = "7.0.6";
+        modDirVersion = version;
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+          hash = "sha256-y6REQKpXr/18ISQdxbwjSw31PEmfj/w+vCkN0zkKdSM=";
+        };
+      };
+    };
+  in {
     # ZFS Specific settings
     boot.supportedFilesystems = ["zfs"];
 
     # ZFS-compatible kernel here
-    boot.kernelPackages = pkgs.linuxPackages_6_18;
-
-    # NOTE: Temporarily set to 2_4 on unstable because it's needed
-    # for linux 6.18 at this time.
-    #
-    # In the future this line below for zfs_2_4 pinning needs to
-    # be commented out/removed.
-    boot.zfs.package = pkgs.unstable.zfs_2_4;
+    boot.kernelPackages = pkgs.linuxPackagesFor linux_7_0_6;
 
     boot.zfs.devNodes = lib.mkDefault "/dev/disk/by-id";
 
