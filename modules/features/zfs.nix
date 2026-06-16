ad8eceebcc566de0162857dea82c53ca1f5999bd
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-06-16 10:12:45 +0200

    Linux 7.0+ for jellyfin and ZFS hosts, removed pin from dockerhost

--- modules/features/zfs.nix
+++ modules/features/zfs.nix
@@ -4,25 +4,23 @@
     pkgs,
     ...
   }: let
-    # NOTE: Temporarily pinned since nixpkgs has version 7.0.9 which has a broken
-    # mediatek bluetooth driver
-    # https://community.frame.work/t/bluetooth-is-borked-cant-init-device-hci0-invalid-argument-22/82605/6
-    linux_7_0_6 = pkgs.linux_7_0.override {
-      argsOverride = rec {
-        version = "7.0.6";
-        modDirVersion = version;
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-          hash = "sha256-y6REQKpXr/18ISQdxbwjSw31PEmfj/w+vCkN0zkKdSM=";
-        };
-      };
-    };
+    # NOTE: In case you need to pin a specific version of the linux kernel, EXAMPLE:
+    # linux_7_0_6 = pkgs.linux_7_0.override {
+    #   argsOverride = rec {
+    #     version = "7.0.6";
+    #     modDirVersion = version;
+    #     src = pkgs.fetchurl {
+    #       url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+    #       hash = "sha256-y6REQKpXr/18ISQdxbwjSw31PEmfj/w+vCkN0zkKdSM=";
+    #     };
+    #   };
+    # };
   in {
     # ZFS Specific settings
     boot.supportedFilesystems = ["zfs"];
 
     # ZFS-compatible kernel here
-    boot.kernelPackages = pkgs.linuxPackagesFor linux_7_0_6;
+    boot.kernelPackages = pkgs.linuxPackages_7_0;
 
     boot.zfs = {
       forceImportRoot = false;
--- modules/hosts/jellyfin/configuration.nix
+++ modules/hosts/jellyfin/configuration.nix
@@ -29,7 +29,7 @@
       ];
     };
 
-    boot.kernelPackages = pkgs.linuxPackages_6_18;
+    boot.kernelPackages = pkgs.linuxPackages_7_0;
 
     # Required for our user
     users.users.tk.extraGroups = [
