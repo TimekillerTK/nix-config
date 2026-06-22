ac381341f331f580bb77ca7911668baa84b56276
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-06-22 09:05:31 +0200

    wip

--- modules/users/builder.nix
+++ modules/users/builder.nix
@@ -1,8 +1,10 @@
 {...}: {
   # Builder user intended for machines where you can send remote builds to
   flake.modules.nixos.builder = {
+    users.groups.builder = {};
     users.users.builder = {
       isSystemUser = true;
+      group = "builder";
       extraGroups = ["wheel"]; # FIXME: probably too board, check later
       openssh.authorizedKeys.keys = [
         (builtins.readFile ../../pub_keys/rpi.pub)
