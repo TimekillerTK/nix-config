fd9204662240893190a3a7c106eb8de0ce19d450
TimekillerTK <8354689-TimekillerTK@users.noreply.gitlab.com>
2026-07-27 15:28:43 +0200

    Work in progress - flooficus is his name

--- modules/hosts/flooficus/configuration.nix
+++ modules/hosts/flooficus/configuration.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: let
-  hostName = "incus";
+  hostName = "flooficus";
 in {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" hostName;
 
@@ -9,13 +9,28 @@ in {
     ...
   }: {
     imports = [
+      # Filesystems on this host are defined with disko
+      inputs.disko.nixosModules.default
+      ./_disko.nix
+
       inputs.self.modules.nixos.system-minimal
+      inputs.self.modules.nixos.prometheus-node-desktop
+      inputs.self.modules.nixos.zfs
+
       inputs.self.modules.nixos.home-manager
       inputs.self.modules.nixos.tk
-
-      inputs.self.modules.nixos.prometheus-node-desktop
     ];
+    home-manager.users.tk = {
+      imports = [
+        inputs.self.modules.homeManager.system-minimal
+      ];
+      # Normal home-manager config stuff goes here
+    };
+
+    # Hostname
+    networking.hostName = hostName;
   };
+
   # Adding this host to the prometheus targets for the grafana host
   flake.modules.nixos.grafana = {
     prometheusTargets = {
