a2184c0ee5655e9e3ea756adab013812e531955e
TimekillerTK <8354689-TimekillerTK@users.noreply.gitlab.com>
2026-08-01 14:44:18 +0200

    removed old disko.nix file for flooficus; added nofail mount option for boot partitions which would prevent the system from booting if one disk fails

--- modules/hosts/flooficus/_disko.nix
+++ modules/hosts/flooficus/_disko.nix
@@ -14,6 +14,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["nofail"];
               };
             };
             zfs = {
@@ -46,6 +47,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot-fallback";
+                mountOptions = ["nofail"];
               };
             };
             zfs = {
