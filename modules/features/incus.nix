6209ed90f97c804235f1b261e65e7e243731789f
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-08-02 11:23:55 +0200

    Setting some best practices for Incus with NixOS

--- modules/features/incus.nix
+++ modules/features/incus.nix
@@ -5,14 +5,25 @@
       ui.enable = true;
       preseed = {
         config = {
+          # This makes the Incus WebUI listen on port 8443
           "core.https_address" = "0.0.0.0:8443";
         };
       };
     };
-
     # Incus on NixOS is unsupported using iptables, therefore
     networking.nftables.enable = true;
 
+    # NOTE: nftables ruleset will be flushed with any change to nftables rules, this results
+    # in the Incus ruleset table (named "incus") being wiped, resulting in loss of
+    # connectivity across VMs and containers.
+    # source: https://wiki.nixos.org/wiki/Incus
+    networking.nftables.flushRuleset = false;
+
+    # WebUI
     networking.firewall.allowedTCPPorts = [8443];
+
+    # NOTE: By default the NixOS firewall will block DHCP requests to the Incus network
+    # source: https://wiki.nixos.org/wiki/Incus
+    networking.firewall.trustedInterfaces = ["incusbr0"];
   };
 }
