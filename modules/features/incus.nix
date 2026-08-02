4abd8864e105d537c86701b70babe101f35fe905
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-08-02 12:39:07 +0200

    Probably not needed

--- modules/features/incus.nix
+++ modules/features/incus.nix
@@ -30,6 +30,6 @@
 
     # NOTE: By default the NixOS firewall will block DHCP requests to the Incus network
     # source: https://wiki.nixos.org/wiki/Incus
-    networking.firewall.trustedInterfaces = ["incusbr0" "docker0"];
+    networking.firewall.trustedInterfaces = ["incusbr0"];
   };
 }
