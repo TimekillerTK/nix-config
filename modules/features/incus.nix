7821f40a145257e7ddbd48ff7b246f8ac690f8fe
TimekillerTK <38417175+TimekillerTK@users.noreply.github.com>
2026-08-02 10:25:38 +0200

    Added preseed with listening address

--- modules/features/incus.nix
+++ modules/features/incus.nix
@@ -3,6 +3,11 @@
     virtualisation.incus = {
       enable = true;
       ui.enable = true;
+      preseed = {
+        config = {
+          "core.https_address" = "0.0.0.0:8443";
+        };
+      };
     };
 
     # Incus on NixOS is unsupported using iptables, therefore
