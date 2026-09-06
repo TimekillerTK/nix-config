07ba003155218ac1fa3c8c06e28353784968e8b8
TimekillerTK <8354689-TimekillerTK@users.noreply.gitlab.com>
2026-09-06 11:32:10 +0200

    (wip) setting incus default network to incusbr0

--- modules/features/incus.nix
+++ modules/features/incus.nix
@@ -26,6 +26,10 @@
             config = {
               "security.secureboot" = false;
             };
+            devices.eth0 = {
+              type = "nic";
+              network = "incusbr0";
+            };
             devices.root = {
               type = "disk";
               path = "/";
